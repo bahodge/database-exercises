38b7cce74b7368c00ce123f865918a07d64fbaa7
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-03-16 16:33:50 -0500

    added select exercises as well as updated album migration and album seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,11 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(50) NOT NULL,
-  name VARCHAR(50),
-  release_date DATE,
-  sales DECIMAL(10, 2),
-  genre TEXT NOT NULL,
+  artist VARCHAR(255) NOT NULL,
+  album VARCHAR(255) NOT NULL,
+  release_date INT UNSIGNED NOT NULL,
+  genre VARCHAR (255) NOT NULL,
+  certified_copies DECIMAL(3,1) NOT NULL,
+  claimed_sales INT UNSIGNED NOT NULL,
   PRIMARY KEY (id)
 );
