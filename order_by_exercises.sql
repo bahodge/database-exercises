29868c9d7b2afac7a9ae70c1d4765896c9b9d1b1
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-03-19 16:57:57 -0500

    added order_by_exercises.sql

---
+++ order_by_exercises.sql
@@ -0,0 +1,35 @@
+USE employees;
+# 1 File CREATED
+
+# 2 SKIPPED, BROKEN! :(
+
+# 3
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name ASC, last_name ASC;
+
+# 4
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name ASC, first_name ASC;
+
+# 5
+SELECT *
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no ASC;
+
+
+# 6
+SELECT *
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no DESC;
+
+# 7
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25'
+ORDER BY hire_date DESC, birth_date DESC;
